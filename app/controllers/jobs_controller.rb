9efdb020cdb8004ff9976a27b7afbe96e75d53aa
EC2 Default User <ec2-user@ip-172-31-17-253.us-east-2.compute.internal>
2021-04-17 08:39:38 +0000

    [Feature][JobsDetail] frontend integration

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,6 @@
 class JobsController < ApplicationController
     def show
-        jobs_id = params[:id]
-        job = ::Job.show(jobs_id)
-        
+        job = ::Job.show(params[:id])
         render 'show', locals: { job: job[:data] }
     end
 end
