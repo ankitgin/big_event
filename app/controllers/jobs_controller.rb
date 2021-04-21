04f8fb03bd882e13dfb60c54f85528ceab0e2cd1
EC2 Default User <ec2-user@ip-172-31-17-253.us-east-2.compute.internal>
2021-04-21 03:01:28 +0000

    [feature][job_update] update the job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,4 +4,9 @@ class JobsController < ApplicationController
     job = ::Job.show(params[:id])
     render 'show', locals: { job: job[:data] }
   end
+  
+  def update
+    job = ::Job.update(params)
+    redirect_to(job_path(id: job[:data][:JobNumber]))
+  end
 end
