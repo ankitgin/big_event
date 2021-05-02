f986a5a80d015b90ed40a69947074b8bbf963378
EC2 Default User <ec2-user@ip-172-31-32-191.ap-south-1.compute.internal>
2021-05-02 06:47:37 +0000

    edit job feature v1

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,9 +4,23 @@ class JobsController < ApplicationController
     job = ::Job.show(params[:id])
     render 'show', locals: { job: job[:data] }
   end
+  
+  def edit
+    job = ::Job.show(params[:id])
+    all_status = ::Job.unique_status()
+    partnerships = ::Job.all_partnerships
+    render 'edit', locals: { job: job[:data], all_status: all_status, partnerships: partnerships }
+  end
 
   def update
-    job = ::Job.update(params)
+    job = ::Job.update(edit_job_params)
     redirect_to(job_path(id: job[:data][:JobNumber]))
+  end
+  
+  private
+  # Making "internal" methods private is not required, but is a common practice.
+  # This helps make clear which methods respond to requests, and which ones do not.
+  def edit_job_params
+    params.permit(:FirstName, :JobNumber, :PhoneNumber, :BestTimeToCall, :Partnership, :Status, :Address, :JobDescription, :Comment)
   end
 end
