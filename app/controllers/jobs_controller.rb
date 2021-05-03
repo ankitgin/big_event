30adfb8f2e6000b88da09c5438c9247335192e49
EC2 Default User <ec2-user@ip-172-31-17-253.us-east-2.compute.internal>
2021-05-03 22:09:48 +0000

    feature/edit_job_form add all fields in the form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -56,7 +56,23 @@ class JobsController < ApplicationController
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def edit_job_params
-    params.permit(:FirstName, :JobNumber, :PhoneNumber, :BestTimeToCall, :Partnership, :Status, :Address, :JobDescription, :Comment, :S, :M, :L, :XL, :XXL, 'Parking Pass Required', 'Completable in Rain', 'All tools provided', 'Ordering T-Shirts', 'PaintGallons Needed', 'PaintGallons Provided', 'PaintBrushes Provided', 'PaintBrushes Needed', 'PaintRollers Provided', 'PaintRollers Needed', 'PaintScrapers Provided', 'PaintScrapers Needed', 'PaintTrays Provided', 'PaintTrays Needed', 'RollerExtensions Provided', 'RollerExtensions Needed', 'Ladders Provided', 'Ladders Needed', 'DropCloths Provided', 'DropCloths Needed', 'TrashBags Provided', 'TrashBags Needed', 'Mops Provided', 'Mops Needed', 'Brooms Provided', 'Brooms Needed', 'DustPans Provided', 'DustPans Needed', 'PaperTowels Provided', 'PaperTowels Needed', 'ScouringPads Provided', 'ScouringPads Needed', 'GardenSpades Provided', 'GardenSpades Needed', 'HandClippers Provided', 'HandClippers Needed', 'Hammers Provided', 'Hammers Needed', 'Saws Provided', 'Saws Needed', 'HardRakes Provided', 'HardRakes Needed', 'LeafRakes Provided', 'LeafRakes Needed', 'Hoes Provided', 'Hoes Needed', 'HedgeShears Provided', 'HedgeShears Needed', 'SpadeShovels Provided', 'SpadeShovels Needed', 'FlatShovels Provided', 'FlatShovels Needed', 'TrenchingShovels Provided', 'TrenchingShovels Needed', 'TreeTrimmers Provided', 'TreeTrimmers Needed', 'Axes Provided', 'Axes Needed', 'Gloves Provided', 'Gloves Needed')
+    params.permit(:FirstName, :JobNumber, :PhoneNumber, :BestTimeToCall, :Partnership, :Status, :Address, :JobDescription, 
+    :Comment, :S, :M, :L, :XL, :XXL, 'Parking Pass Required', 'Completable in Rain', 'All tools provided', 'Ordering T-Shirts', 
+    'PaintGallons Needed', 'PaintGallons Provided', 'PaintBrushes Provided', 'PaintBrushes Needed', 'PaintRollers Provided', 
+    'PaintRollers Needed', 'PaintScrapers Provided', 'PaintScrapers Needed', 'PaintTrays Provided', 'PaintTrays Needed', 
+    'RollerExtensions Provided', 'RollerExtensions Needed', 'Ladders Provided', 'Ladders Needed', 'DropCloths Provided', 
+    'DropCloths Needed', 'TrashBags Provided', 'TrashBags Needed', 'Mops Provided', 'Mops Needed', 'Brooms Provided', 'Brooms Needed', 
+    'DustPans Provided', 'DustPans Needed', 'PaperTowels Provided', 'PaperTowels Needed', 'ScouringPads Provided', 'ScouringPads Needed', 
+    'GardenSpades Provided', 'GardenSpades Needed', 'HandClippers Provided', 'HandClippers Needed', 'Hammers Provided', 'Hammers Needed', 
+    'Saws Provided', 'Saws Needed', 'HardRakes Provided', 'HardRakes Needed', 'LeafRakes Provided', 'LeafRakes Needed', 'Hoes Provided', 
+    'Hoes Needed', 'HedgeShears Provided', 'HedgeShears Needed', 'SpadeShovels Provided', 'SpadeShovels Needed', 'FlatShovels Provided', 
+    'FlatShovels Needed', 'TrenchingShovels Provided', 'TrenchingShovels Needed', 'TreeTrimmers Provided', 'TreeTrimmers Needed', 
+    'Axes Provided', 'Axes Needed', 'Gloves Provided', 'Gloves Needed', 'Yard Sign', 'Years participated in The Big Event',
+    'Recipient Biography', 'Is there a pet at the residence?', 'If yes, what type of pet?', 'Overall, is there a risk?',
+    'If so, what is the risk?', 'Is this a media job?', 'Is Job Site Checklist complete?', 'Is Risk Eval Complete?', 
+    'Is Recipient Biography complete?', 'Is Waiver/Indemnation Form Complete?', "Is Waiver/Indemnation Form Complete?",
+    'Is job reviewed by exec?', 'Backup job?', 'See Solutions?', 'Is Checked In', 'Is Job Cancelled', 'Cancellation Date',
+    'Cancellation Reason')
   end
 
   def check_login
